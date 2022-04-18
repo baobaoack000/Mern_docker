7f8f3c54fe8d75f81648e2ab43d1cdbf8c7df1d8
Nguyen Gia Bao <Baobaoack000@gmail.com>
2022-04-18 11:04:15 +0700

    feature/[Backend] install snyk + sentry

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,37 @@
+// Sentry Init
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
+
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+// Sentry Init
+Sentry.init({
+  dsn: "https://4487e8f71af44be78f941422ca9ea118@o1205122.ingest.sentry.io/6341285",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
+
+// default
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
