3e9d4a1795b22f5c9b498878f7f4cfb142f87f2b
Nguyen Gia Bao <Baobaoack000@gmail.com>
2022-05-05 22:01:37 +0700

    feature/[Frontend] install tailwinds

---
+++ frontend/src/Hello.js
@@ -0,0 +1,13 @@
+import { graphql, buildSchema } from "graphql";
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => "Hello world!" };
+
+graphql(schema, "{ hello }", root).then((response) => {
+  console.log(response);
+});
