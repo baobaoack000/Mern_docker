2243df49372e1b0cdf4d53026c176c3a0ff24f63
Nguyen Gia Bao <Baobaoack000@gmail.com>
2022-05-10 09:19:31 +0700

    hotfix/ server.js Sentry

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
 // Sentry Init
-const Sentry = require('@sentry/node');
+const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
 
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-
 // Sentry Init
 Sentry.init({
-  dsn: "https://4487e8f71af44be78f941422ca9ea118@o1205122.ingest.sentry.io/6341285",
+  dsn: "https://380c88230d384f0192b471d528c5bf1d@o1205122.ingest.sentry.io/6388991",
   integrations: [
     // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
@@ -29,17 +26,21 @@ Sentry.init({
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
-app.use(Sentry.Handlers.errorHandler());
 
+// All controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
 // default
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
-
