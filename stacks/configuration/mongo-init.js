0a1407f1b4c670dad4bb3c2bc62d8415ab4bdfa4
Nguyen Gia Bao <Baobaoack000@gmail.com>
2022-06-08 17:14:02 +0700

    [Backend] Fixing appsmith db issues

--- stacks/configuration/mongo-init.js
+++ stacks/configuration/mongo-init.js
@@ -2,13 +2,13 @@ let error = false
 print("**** Going to start Mongo seed ****")
 
 var admin = db.getSiblingDB("admin")
-admin.auth("appsmith", "VRMRVRh2QxW8Q")
+admin.auth("appsmith", "JLsDuOEFwmX6Z")
 
 let res = [
     db.createUser(
         {
             user: "appsmith",
-            pwd: "VRMRVRh2QxW8Q",
+            pwd: "JLsDuOEFwmX6Z",
             roles: [{
                 role: "root",
                 db: "admin"
